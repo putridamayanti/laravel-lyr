4e93126fc12d3f01abbf3551f392afb6a055e579
Putri Damayanti <damayartdesign@gmail.com>
2023-01-11 18:10:49 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/version', function () {
+    return response()->json([
+        'status' => true,
+        'data' => 'Test API v1.0.0'
+    ], 200);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
